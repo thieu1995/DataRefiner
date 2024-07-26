4591cbf0858ff7fd2aaf435274ce63836f7da0e1
Thieu Nguyen <nguyenthieu2102@gmail.com>
2024-07-26 14:46:59 +0700

    Fix tests

--- tests/test_one_hot_encoder.py
+++ tests/test_one_hot_encoder.py
@@ -4,8 +4,8 @@
 #       Github: https://github.com/thieu1995        %                         
 # --------------------------------------------------%
 
-import pytest
 import numpy as np
+import pytest
 from datarefiner import OneHotEncoder
 
 
@@ -16,14 +16,17 @@ def one_hot_encoder():
     ohe.fit(data)
     return ohe
 
+
 def test_one_hot_encoder_transform(one_hot_encoder):
     data = np.array([['cat'], ['dog'], ['cat'], ['fish'], ['dog']])
     encoded_data = one_hot_encoder.transform(data)
     assert np.array_equal(encoded_data, np.array([[1, 0, 0], [0, 1, 0], [1, 0, 0], [0, 0, 1], [0, 1, 0]]))
 
+
 def test_one_hot_encoder_categories(one_hot_encoder):
     categories = one_hot_encoder.categories_
     assert np.array_equal(categories, [np.array(['cat', 'dog', 'fish'], dtype=object)])
+
 
 if __name__ == "__main__":
     pytest.main()
